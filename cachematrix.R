1dcb9110000d47657f4f1455e2211f28627c4169
Fredol99 <62766658+Fredol99@users.noreply.github.com>
2020-03-27 19:24:15 +0100

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
+##############################################
+########## Programming Assignment 2 ##########
+##############################################
 
-## Write a short comment describing this function
+## The following functions work as the functions given in
+## the assignment instructions : the syntax and the role of
+## each function is quite the same
+
+## These functions worked well on some examples given
+## in the course discussion
+
 
 makeCacheMatrix <- function(x = matrix()) {
+        
+        inv <- NULL
+        
+        set <- function(y) { # Sets the matrix's value to y
+                m <<- y
+                inv <<- NULL
+        }
+        
+        get <- function() m # Returns the matrix m
+        
+        setinv <- function(solve) inv <<- solve # Sets the 'inv' value to
+        # the matrix's inverse
+        
+        getinv <- function() inv # Allows to get the value of 'inv'
+        
+        list(set = set, get = get, # Returns the CacheMatrix object
+             setinv = setinv,
+             getinv = getinv)
 
 }
 
 
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        
+        inv <- m$getinv() # Checks the current value of inv
+        
+        if(!is.null(inv)) { # If the inverse have already been calculated...
+                message("getting cached data")
+                return(inv) # ... then returns its value 
+        }
+        
+        data <- m$get() # Else get the value of the matrix m
+        inv <- solve(data, ...) # Compute its inverse...
+        m$setinv(inv) # ... and store it in the CacheMatrix object
+        inv
 }
